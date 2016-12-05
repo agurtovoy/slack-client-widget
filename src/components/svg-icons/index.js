cd76e2a9e3d1038d4dc119a65c962ee8371b2021
Aleksey Gurtovoy <agurtovoy@acm.org>
2016-12-04 21:46:13 -0600

    Channels sidebar: channel hash, active channel highlight + related cosmetic fixes

--- src/components/svg-icons/index.js
+++ src/components/svg-icons/index.js
@@ -1,9 +1,11 @@
 import BellIcon from './assets/bell.inline.svg';
 import ArrowDownIcon from './assets/arrow-down.inline.svg';
 import QuickSwitcherIcon from './assets/quick-switcher.inline.svg';
+import ChannelHashIcon from './assets/channel-hash.inline.svg';
 
 export {
     BellIcon,
     ArrowDownIcon,
-    QuickSwitcherIcon
+    QuickSwitcherIcon,
+    ChannelHashIcon
     };
--- src/containers/channels-container/components/channel-entry/channel-entry.js
+++ src/containers/channels-container/components/channel-entry/channel-entry.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { ChannelHashIcon } from '../../../../components/svg-icons';
 import styles from './channel-entry.css';
 
 export default ( { channel } ) =>
     <li className={styles.default}>
-        <a className={styles.name}>{channel.name}</a>
+        <a className={ styles[channel.active ? 'active' : 'name'] }>
+            <ChannelHashIcon className={styles.icon} />
+            <span className={styles.text}>{channel.name}</span>
+        </a>
     </li>;
