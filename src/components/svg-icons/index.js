301fc73b910e992633a1aedffd6d40b83b90331a
Aleksey Gurtovoy <agurtovoy@acm.org>
2016-12-04 19:17:13 -0600

    Channels sidebar: finish "quick switcher"

--- src/components/svg-icons/index.js
+++ src/components/svg-icons/index.js
@@ -1,7 +1,9 @@
 import BellIcon from './assets/bell.inline.svg';
 import ArrowDownIcon from './assets/arrow-down.inline.svg';
+import QuickSwitcherIcon from './assets/quick-switcher.inline.svg';
 
 export {
     BellIcon,
-    ArrowDownIcon
+    ArrowDownIcon,
+    QuickSwitcherIcon
     };
--- src/containers/quick-switcher/quick-switcher.js
+++ src/containers/quick-switcher/quick-switcher.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import { QuickSwitcherIcon } from '../../components/svg-icons';
 import styles from './quick-switcher.css';
 
 export default ( { team, user } ) =>
     <div className={styles.default}>
+        <QuickSwitcherIcon className={styles.icon} />
+        <span className={styles.label}>Quick Switcher</span>
+        <span className={styles.shortcut}>⌘K</span>
     </div>;
