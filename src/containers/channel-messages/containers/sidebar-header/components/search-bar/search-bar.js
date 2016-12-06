eb346bb5ed14319ba70a7c98b02c4a87559b5cac
Aleksey Gurtovoy <agurtovoy@acm.org>
2016-12-06 02:31:31 -0600

    Search bar stub

---
+++ src/containers/channel-messages/containers/sidebar-header/components/search-bar/search-bar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+// import { EditIcon, PlusIcon, UserIcon } from '../../../../../../components/svg-icons';
+import styles from './search-bar.css';
+
+export default ( { channel } ) =>
+    <div className={styles.default}>
+    </div>;
--- src/containers/channel-messages/containers/sidebar-header/sidebar-header.js
+++ src/containers/channel-messages/containers/sidebar-header/sidebar-header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import SearchBar from './components/search-bar';
 import { VerticalEllipsesIcon, StarIcon, MentionIcon } from '../../../../components/svg-icons';
 import styles from './sidebar-header.css';
 
 export default ( { channel } ) =>
     <div className={styles.default}>
+        <SearchBar />
         <a className={styles.mentions} href="#"><MentionIcon /></a>
         <a className={styles.stars} href="#"><StarIcon /></a>
         <a className={styles.more} href="#"><VerticalEllipsesIcon /></a>
