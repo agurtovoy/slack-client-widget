c17a943e45ba193bfd97f83e08ad291cbac44a1d
Aleksey Gurtovoy <agurtovoy@acm.org>
2016-12-08 02:18:40 -0600

    Fix textarea attrs

--- src/containers/channel-messages/containers/message-input/message-input.js
+++ src/containers/channel-messages/containers/message-input/message-input.js
@@ -4,6 +4,6 @@ import styles from './message-input.css';
 
 export default ( { channel } ) =>
     <div className={styles.default}>
-        <textarea className={styles.input} autocorrect="off" autocomplete="off" spellcheck="true" placeholder={"Message #" + channel.name} />
+        <textarea className={styles.input} autoCorrect="off" autoComplete="off" spellCheck="true" placeholder={"Message #" + channel.name} />
         <a className={styles.upload}><NewUploadIcon className={styles.icon}/></a>
     </div>;
