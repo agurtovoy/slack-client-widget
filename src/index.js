262e8e8d307d88879d0bb2457626f08506197323
Aleksey Gurtovoy <agurtovoy@acm.org>
2016-12-09 22:18:01 -0600

    Set up redux, react-redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+
 import SlackClientWidget from './slack-client-widget';
 import 'suitcss-base';
 import './index.less';
 
+
+//import * as reducers from './store/reducers';
+//const store = createStore(combineReducers(reducers), applyMiddleware(thunk));
+const store = createStore( ( state ) => state );
+
+
 ReactDOM.render(
-  <SlackClientWidget />,
-  document.getElementById('root')
+    <Provider store={store}>
+        <SlackClientWidget />
+    </Provider>,
+    document.getElementById('root')
 );
